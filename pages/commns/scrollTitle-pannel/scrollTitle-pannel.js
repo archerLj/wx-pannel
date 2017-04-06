70ddba62933bb1a41a05eaaea1b703cd53bd0a3d
archerLj <lj0011977@163.com>
2017-04-06 13:23:52 +0800

    Update scrollTitle-pannel.js

--- pages/commns/scrollTitle-pannel/scrollTitle-pannel.js
+++ pages/commns/scrollTitle-pannel/scrollTitle-pannel.js
@@ -1,8 +1,12 @@
+// 组件变量及数据
 let _compData = {
   '__stpannel__.curIndex': 0
 }
 
+
+// 组件事件
 let _compEvent = {
+  // 按钮方法以及回掉方法调用
   __stpannel__clickText: function(e) {
     this.setData({'__stpannel__.curIndex': e.target.dataset.index})
     typeof this.scrollTitlePannel._configs.callback == "function" &&
@@ -13,34 +17,32 @@ let _compEvent = {
 let scrollTitlePannel = {
   init: function(data) {
     if (data) {
+      // 给回掉方法赋值
       Object.assign(this._configs, data)
     }
   }
 }
 
 function ScrollTitlePannel () {
-  // 定义组件的一些回调
+  // 回调方法
   this._configs = {
     callback: null
   }
-  // 拿到当前页面对象
+
+  // 当前页面对象
   let pages = getCurrentPages()
   let curPage = pages[pages.length - 1]
 
-  // 把组件的事件“合并到”页面对象上
+  // 把组件的事件赋到当前页面上
   Object.assign(curPage, _compEvent)
-
   this.__page = curPage
 
-  // 附加到page上，方便访问
-
+  // 将scrollTitlePannel赋到当前页面，方便后面使用
   Object.assign(this, scrollTitlePannel)
-
   curPage.scrollTitlePannel = this
 
-  // 把组件的数据“注入”到页面的data对象中
+  // 把组件的变量数据赋给当前页面的data
   curPage.setData(_compData)
-
   return this
 }
 
